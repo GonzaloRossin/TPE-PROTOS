1d9076a6ba86caf2a7359e95598938896d0a4d4d
Uriel <umihura@itba.edu.ar>
2022-06-01 00:10:25 -0300

    Apply coda patches

--- coda_starting_files/src/main.c
+++ coda_starting_files/src/main.c
@@ -23,9 +23,9 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 
-#include "socks5.h"
+//include "socks5.h" //comento esto porque no lo dieron
 #include "selector.h"
-#include "socks5nio.h"
+//#include "socks5nio.h" //comento esto porque no lo dieron
 
 static bool done = false;
 
